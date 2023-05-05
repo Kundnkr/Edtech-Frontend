6ac0df838a22d328730dfb46a8d3bd13ebb5089f
kundan Kumar <kkundan501@gmail.com>
2023-05-05 18:33:39 +0530

    first

--- src/Assets/Components/NavBar.jsx
+++ src/Assets/Components/NavBar.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AIM from '../Images/AIM.png'
 
 export default function NavBar() {
     return (
         <div>
             <header className='nav'>
                 <div className="logo">
-                    <Link to="/">Logo</Link>
+                    <img src={AIM} alt="" />
                 </div>
                 <div className="items">
                     <Link to="/profilecomplete">Student Data </Link>
                     <Link to="/profilemgmt">Profile</Link>
                     <Link to="/sign-up">Sign Up</Link>
-                    
                     <Link to="/login">Login</Link>
-                    <Link to="/change_password">Change Password</Link>
                 </div>
             </header>
         </div>
