42d63997f2af18f6566167008d1b632cf219f333
kundan Kumar <kkundan501@gmail.com>
2023-05-06 16:17:57 +0530

    first

--- src/Assets/Components/Register.jsx
+++ src/Assets/Components/Register.jsx
@@ -3,6 +3,10 @@ import { useState } from 'react'
 import axios from 'axios'
 import '../Stylesheet/Regsiter.css';
 import AIM from '../Images/AIM.png'
+import keyboy from '../Images/keyboy.png'
+import profilepic from '../Images/profilePic.gif'
+import loginsecurity from '../Images/login-security.png'
+import market from '../Images/market.png'
 import { useNavigate } from 'react-router-dom';
 
 let email = "";
@@ -33,15 +37,15 @@ export default function Register() {
             last_name: data.last_name,
             email: data.email,
             phone: data.phone,
-            password:data.password,
-            confirm_password:data.confirm_password
+            password: data.password,
+            confirm_password: data.confirm_password
         }
-        
+
         email = data.email;
         axios.post('https://testapp-9prn.onrender.com/api/register/', userdata).then((res) => {
             let registrationForm = document.getElementById('form');
             alert(res.data.msg);
-            if(res.data.status===200){
+            if (res.data.status === 200) {
                 Navigate('/otp-verify');
             }
             registrationForm.reset();
@@ -58,34 +62,35 @@ export default function Register() {
             <section className="registration-box">
                 <div className="content">
                     <div className="disc">
-                        <h1>Registration page</h1>
+                        <img className='keyboy' src={profilepic} alt="" />
+                        <img className='aimLogo' src={AIM} alt="" /> 
                     </div>
                 </div>
                 <div id='registration-from' onSubmit={handlesubmit} >
                     <div className="img">
-                    <img src={AIM} alt="" />
+                        
                     </div>
                     <form id='form' className="form-item">
-                
+
                         <input required type="text" name='username' id='username' placeholder='Username' onChange={handlechange} />
-                        
-                       
+
+
                         <input required type="text" name="first_name" id="first_name" maxlength="20" pattern="[A-Za-z.]{1,20}" placeholder='First Name' onChange={handlechange} />
 
-                        
+
                         <input required type="text" name='last_name' maxlength="20" pattern="[A-Za-z]{1,20}" placeholder='Last Name' onChange={handlechange} />
 
-                        
+
                         <input required type="email" name='email' placeholder='Email' onChange={handlechange} />
 
-                        
+
                         <input required type="tel" name='phone' minlength="10" maxlength="10" pattern="[0-9]{1,10}" placeholder='Mobile' onChange={handlechange} />
 
-                        
-                        <input required type="password" minlength="6" maxlength="15" name='password' placeholder='Password' onChange={handlechange}/>
 
-                        
-                        <input required type="password" minlength="6" maxlength="15" name='confirm_password' placeholder='Confirm Password' onChange={handlechange}/>
+                        <input required type="password" minlength="6" maxlength="15" name='password' placeholder='Password' onChange={handlechange} />
+
+
+                        <input required type="password" minlength="6" maxlength="15" name='confirm_password' placeholder='Confirm Password' onChange={handlechange} />
                         <button>REGISTER</button>
                     </form>
                 </div>
@@ -94,4 +99,4 @@ export default function Register() {
     )
 }
 
-export {email};
+export { email };
